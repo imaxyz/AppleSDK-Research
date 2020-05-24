3c0b6e863de66c87ee9f3f47257c036ef85ce6d4
imaxyz <YI201610@users.noreply.github.com>
2020-05-24 23:40:30 +0900

    SwiftUIの動作確認コードを追加

---
+++ src/AppleSDKTips/Application/Presentation/SwiftUI/HelloSwiftUIView.swift
@@ -0,0 +1,22 @@
+//
+//  HelloSwiftUIView.swift
+//  AppleSDKTips
+//
+//  Created by imaxyz on 2020/05/24.
+//  Copyright © 2020 imaxyz. All rights reserved.
+//
+
+import SwiftUI
+
+// 要iOS13以降
+struct HelloSwiftUIView: View {
+    var body: some View {
+        Text("Hello, SwiftUI!")
+    }
+}
+
+struct HelloSwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        HelloSwiftUIView()
+    }
+}
