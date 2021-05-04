40621bacc12619cb1802fd20c12d9f537eaa0093
Yuji Imamura <23151008+imaxyz@users.noreply.github.com>
2021-05-04 22:15:26 +0900

    Facebook SDKを実行するコードを整理 (#12)
    * Facebook関連処理を別クラスに委譲
    
    * コメントを追加
    
    * コメントを修正
    
    * facebook sdkを実行するコードを追加
    
    Co-authored-by: imaxyz <imaxyz@users.noreply.github.com>

---
+++ src/AppleSDKTips/Framework/Application/FacebookDelegate.swift
@@ -0,0 +1,35 @@
+//
+//  FacebookDelegate.swift
+//  AppleSDKTips
+//
+//  Created by imaxyz on 2021/05/04.
+//  Copyright © 2021 imaxyz. All rights reserved.
+//
+
+import Foundation
+import FBSDKCoreKit
+
+class FacebookDelegate: NSObject {
+    
+    class func start_sdk(application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Void {
+        
+        // Facebook SDKのバージョンをログに出力
+        print("[FBSDK sdkVersion]" + Settings.sdkVersion)
+        print("[FBSDK defaultGraphAPIVersion]" + Settings.defaultGraphAPIVersion)
+                
+        // 以下のコードは、Facebook SDKにつながっている。Facebook SDKを起動する。
+        ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    class func open_url(application: UIApplication, url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        
+        //Facebook SDKのコードを実行する
+        return ApplicationDelegate.shared.application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
+    }
+    
+    class func did_become_active() {
+        //Facebook SDKのコードを実行する
+        AppEvents.activateApp()
+    }
+}
