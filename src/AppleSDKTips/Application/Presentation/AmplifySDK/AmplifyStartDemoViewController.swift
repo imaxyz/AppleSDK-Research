0c1c8996ed1267c8050327c7e4b0c3f79f5cdcff
imaxyz <imaxyz@users.noreply.github.com>
2021-06-23 09:01:19 +0900

    CRUDを検証する機能を追加

--- src/AppleSDKTips/Application/Presentation/AmplifySDK/AmplifyStartDemoViewController.swift
+++ src/AppleSDKTips/Application/Presentation/AmplifySDK/AmplifyStartDemoViewController.swift
@@ -21,46 +21,8 @@ class AmplifyStartDemoViewController: UIViewController, UITableViewDelegate, UIT
         query_data_store()
     }
     
-    // Todoモデルを問い合わせる
-    @IBAction func query_data_store() {
-        
-        let query_sort = QuerySortInput([QuerySortBy.descending(Todo.keys.description)])
-        Amplify.DataStore.query(Todo.self,
-                                // クエリの条件を付加
-                                where: Todo.keys.priority.eq(Priority.high),
-                                sort: query_sort)
-        { result in
-            
-            switch(result) {
-            case .success(let todos):
-                print("todo count: \(todos.count)")
-              
-                // todo リストの内部メモリを初期化
-                todo_list.removeAll()
-                
-                for todo in todos {
-//                    print("==== Todo ====")
-//                    print("Name: \(todo.name)")
-//                    if let priority = todo.priority {
-//                        print("Priority: \(priority)")
-//                    }
-//                    if let description = todo.description {
-//                        print("Description: \(description)")
-//                    }
-                    
-                    // todoリストにモデルを追加
-                    todo_list.append(todo)
-                }
-                
-                // テーブルを再読み込み
-                todo_list_table_view.reloadData()
-                
-              case .failure(let error):
-                  print("Could not query DataStore: \(error)")
-              }
-        }
-    }
-
+    // MARK: - Button Actions
+    
     @IBAction func did_tap_create_todo_button(_ sender: Any) {
 
         // 現在時刻を取得
@@ -93,6 +55,132 @@ class AmplifyStartDemoViewController: UIViewController, UITableViewDelegate, UIT
         // テーブルを再読み込み
         todo_list_table_view.reloadData()
     }
+    
+    // Todoモデルを問い合わせる
+    @IBAction func query_data_store() {
+        
+        let query_sort = QuerySortInput([QuerySortBy.descending(Todo.keys.description)])
+        Amplify.DataStore.query(Todo.self,
+                                // クエリの条件を付加
+                                where: Todo.keys.priority.eq(Priority.high),
+                                sort: query_sort)
+        { result in
+            
+            switch(result) {
+            case .success(let todos):
+                print("todo count: \(todos.count)")
+              
+                // todo リストの内部メモリを初期化
+                todo_list.removeAll()
+                
+                for todo in todos {
+                    print("==== Todo ====")
+                    print("Name: \(todo.name)")
+                    if let priority = todo.priority {
+                        print("Priority: \(priority)")
+                    }
+                    if let description = todo.description {
+                        print("Description: \(description)")
+                    }
+                    
+                    // todoリストにモデルを追加
+                    todo_list.append(todo)
+                }
+                
+                // テーブルを再読み込み
+                todo_list_table_view.reloadData()
+
+              case .failure(let error):
+                  print("Could not query DataStore: \(error)")
+              }
+        }
+    }
+    
+    /// Todo Updateボタンをタップ
+    /// - Parameter sender: ボタンオブジェクト
+    @IBAction func did_tap_update_todo_button(_ sender: UIButton) {
+        print("did tap update todo button.")
+        
+        // アプリに、所定の条件のTodoデータを、問い合わせる
+        Amplify.DataStore.query(Todo.self,
+                                where: Todo.keys.name.eq("todo model: 2021年6月23日 8:33:01"))
+        { result in
+            
+            switch(result) {
+            case .success(let todos):
+                
+                //
+                guard todos.count >= 1, var updatedTodo = todos.first else {
+                    print("Did not find exactly one todo, bailing")
+                    return
+                }
+                
+                updatedTodo.name = "Updated Todo model"
+                
+                Amplify.DataStore.save(updatedTodo) { result in
+                    
+                    switch(result) {
+                    case .success(let savedTodo):
+                        print("Updated item: \(savedTodo.name)")
+                        
+                        let mainQueue = DispatchQueue.main
+                        let time = DispatchTime.now() + Double(Int64(0.5 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+                        mainQueue.asyncAfter(deadline: time) {
+                            // テーブルを再読み込み
+                            self.query_data_store()
+                        }
+                        
+                    case .failure(let error):
+                        print("Could not update data in DataStore: \(error)")
+                    }
+                }
+                
+            case .failure(let error):
+                print("Could not query DataStore: \(error)")
+            }
+        }
+    }
+    
+    /// Todo Deleteボタンをタップ
+    /// - Parameter sender: ボタンオブジェクト
+    @IBAction func did_tap_delete_todo_button(_ sender: UIButton) {
+        print("did tap delete todo button.")
+
+        Amplify.DataStore.query(Todo.self,
+                                where: Todo.keys.name.eq("Updated Todo model")) { result in
+            switch(result) {
+            case .success(let todos):
+                
+                guard todos.count >= 1, let toDeleteTodo = todos.first else {
+                    print("Did not find exactly one todo, bailing")
+                    return
+                }
+                
+                Amplify.DataStore.delete(toDeleteTodo) { result in
+                    switch(result) {
+                    
+                    case .success:
+                        print("Deleted item: \(toDeleteTodo.name)")
+                        
+                        let mainQueue = DispatchQueue.main
+                        let time = DispatchTime.now() + Double(Int64(0.5 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+                        mainQueue.asyncAfter(deadline: time) {
+                            // テーブルを再読み込み
+                            self.query_data_store()
+                        }
+
+                    
+                    case .failure(let error):
+                        print("Could not update data in DataStore: \(error)")
+                    }
+                }
+            case .failure(let error):
+                print("Could not query DataStore: \(error)")
+            }
+       }
+    }
+    
+    
     
     // MARK: - UITableView
 
